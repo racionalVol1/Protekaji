bc2de89e2d5521c8d6a6885a026b1641317f8f24
Perondi <guilhermeperondi09@gmail.com>
2025-07-01 01:08:12 -0300

    Atualiza .gitignore e remove arquivos ignorados do versionamento

---
+++ Data/Migrations/20250701035347_CreateExtintorTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Protekaji.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateExtintorTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExtintorModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Marca = table.Column<string>(type: "TEXT", nullable: false),
+                    Tipo = table.Column<string>(type: "TEXT", nullable: false),
+                    Capacidade = table.Column<string>(type: "TEXT", nullable: false),
+                    NumeroDeSerie = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExtintorModels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExtintorModels");
+        }
+    }
+}
